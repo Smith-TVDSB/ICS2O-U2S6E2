7371518f5b12a3a8f01f524d1b03f09a581c2e44
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-02-27 14:51:33 -0500

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -2,71 +2,17 @@ import pytest
 import student 
 
 
-
-def test_inputs_specific_output():
-    input_values=['3','4']
-    output=[]
-
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
-
-    student.main()
-
-    assert output[2]==7
-
-def test_inputs_all_output(capsys):
-    input_values=['3','4']
-    output=[]
-
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.main()
+def test_3(capsys):
+    student.pyramid(3)
     out,err =  capsys.readouterr()
-    assert "7" in out
+    assert out == "*\n**\n***" or out == "* \n* * \n* * *\n"
 
+def test_1(capsys):
+    student.pyramid(1)
+    out,err =  capsys.readouterr()
+    assert out == "*\n" or out == "*"
 
-
-#Standard test output ONLY (no input)
-#No need for the if __name__ condition in the main code, but might as well when done so students get used to it.
-"""def test_hello(capsys):
-    import hello
-    out,err = capsys.readouterr()
-    assert out == "Hello world!\n" or "bye" in out, "Should read 'Hello world!' "
-"""
-
-# Tests multiple values one at a time:
-
-# def test_higher():
-#     for i in [19, 91, 20, 24, 54, 33, 18]:
-#         input_values=[str(i)]
-#         output=[]
-
-#         def mock_input(s=None):
-#             if s is not None:
-#                 output.append(s)
-#                 return input_values.pop(0)
-#             else:
-#                 output.append("")
-#                 return input_values.pop(0)
-        
-#         student.input = mock_input
-#         student.print = lambda s : output.append(s)
-
-#         student.main()
-
-#         assert "true" in output[1].lower()
+def test_5(capsys):
+    student.pyramid(5)
+    out,err =  capsys.readouterr()
+    assert out == "*\n**\n***\n****\n******\n" or out == "* \n* * \n* * * \n* * * * \n* * * * * * \n"
